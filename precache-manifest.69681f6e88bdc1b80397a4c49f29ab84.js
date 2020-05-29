1d299fe27512226abe2704bdd37034411e362be4
Marko Gusic <markogusic@Markos-MacBook-Pro.local>
2020-05-29 16:56:05 -0700

    Updates

---
+++ precache-manifest.69681f6e88bdc1b80397a4c49f29ab84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6984396e30627ded113d14ff0ab346e2",
+    "url": "/monsters-rorlodex.git/index.html"
+  },
+  {
+    "revision": "d8f2d55b4662b7b1ac6c",
+    "url": "/monsters-rorlodex.git/static/css/main.39c9b961.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rorlodex.git/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rorlodex.git/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f2d55b4662b7b1ac6c",
+    "url": "/monsters-rorlodex.git/static/js/main.b9a5a942.chunk.js"
+  },
+  {
+    "revision": "cdbd0916f180e97751c1",
+    "url": "/monsters-rorlodex.git/static/js/runtime-main.3fc14fcc.js"
+  }
+]);
